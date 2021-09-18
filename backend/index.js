c4b724b3362978f798b2c7d5e36e05cef7b633c0
JosephDM-17 <josephdominicmathew@gmail.com>
2021-09-18 15:49:31 +0530

    endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,47 @@
 const app = require('express')();
 const mongoose = require('mongoose');
-require('dotenv/config');
-
-app.get('/kochi', (req, res) => {
-res.status(200).send({
- location: 'kochi',
- news: 'Narcotic jihad row: Congress condemns it, BJP says serious issue'
-})
-
-});
-
-//connect to db
-mongoose.connect(process.env.DB_CONNECTION, ()=> console.log('Connected to db'));
-
-
-app.listen(3000);
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  require('dotenv/config');
+  
+  
+  //connect to db
+  mongoose.connect(process.env.DB_CONNECTION,{useNewUrlParser:true}, ()=> console.log('Connected to db'));
+  
+  const newsSchema={
+      title:String,
+      location:String,
+      content:String,
+    };
+    
+    const News=mongoose.model("News",newsSchema);
+    
+    app.get("/news",(req,res)=>{
+      News.find((err,foundNews)=>{
+        if(!err){
+          res.send(foundNews);
+        }else{
+          res.send(err);
+        }
+      });
+    });
+    
+    
+    app.get("/news/:news_location",function(req, res){
+    
+      News.find({location: req.params.news_location}, function(err, found_news){
+        if (found_news) {
+          res.send(found_news);
+        } else {
+          res.send("No match found");
+        }
+      });
+    });
+  
+  app.listen(3000);
+  
+  
+  
+  
