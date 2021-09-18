5dcfd203e86f10c4ffda76c8893c36b0cc9ae9f1
jomey-12 <jomeyjj28@gmail.com>
2021-09-18 14:57:57 +0530

    added detailed news page

--- lib/pages/deatiled_news_page.dart
+++ lib/pages/deatiled_news_page.dart
@@ -30,7 +30,63 @@ class DetailedNews extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * .3,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    image: DecorationImage(
+                      image: AssetImage('assets/image.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "16 Sep 2021   4:30 PM",
+                    style: TextStyle(
+                      color: Colors.black45,
+                    ),
+                  ),
+                  Icon(Icons.bookmark)
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Title1",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
